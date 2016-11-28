a9467a6cfafcd33e2ece74b8fcfdb9829747fe53
scarragher <car07009312@hotmail.co.uk>
2016-11-28 13:46:02 +0000

    1

--- config_test.go
+++ config_test.go
@@ -2,51 +2,58 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"testing"
+
+	"github.com/scarragher/goconfig"
 )
 
 func TestConfigurations(t *testing.T) {
 
-	boolConfig := NewConfiguration("IsTestEnabled", true)
-	stringConfig := NewConfiguration("MyName", "Stephen")
-	intConfig := NewConfiguration("AttemptsAtGoal", 9)
-	float64Config := NewConfiguration("Heat", 14.)
+	boolConfig := config.NewConfiguration("IsTestEnabled", true)
+	stringConfig := config.NewConfiguration("MyName", "Stephen")
+	intConfig := config.NewConfiguration("AttemptsAtGoal", 9)
+	float64Config := config.NewConfiguration("Heat", 14.)
 
 	fmt.Println(boolConfig)
 	fmt.Println(stringConfig)
 	fmt.Println(intConfig)
 	fmt.Println(float64Config)
 
-	AddConfiguration(*boolConfig)
-	AddConfiguration(*stringConfig)
-	AddConfiguration(*intConfig)
-	AddConfiguration(*float64Config)
+	config.AddConfiguration(*boolConfig)
+	config.AddConfiguration(*stringConfig)
+	config.AddConfiguration(*intConfig)
+	config.AddConfiguration(*float64Config)
 
-	boolGConfig, _ := GetConfiguration("IsTestEnabled")
-	stringGConfig, _ := GetConfiguration("MyName")
-	intGConfig, _ := GetConfiguration("AttemptsAtGoal")
-	float64GConfig, _ := GetConfiguration("Heat")
+	boolGConfig, _ := config.GetConfiguration("IsTestEnabled")
+	stringGConfig, _ := config.GetConfiguration("MyName")
+	intGConfig, _ := config.GetConfiguration("AttemptsAtGoal")
+	float64GConfig, _ := config.GetConfiguration("Heat")
 
 	fmt.Println(boolGConfig)
 	fmt.Println(stringGConfig)
 	fmt.Println(intGConfig)
 	fmt.Println(float64GConfig)
 
-	if GetConfigurationValue("IsTestEnabled").(bool) == true {
-		fmt.Println("does the compiler know this is a bool?")
+	if config.GetConfigurationValue("IsTestEnabled").(bool) == true {
+		fmt.Println("OK")
 	}
 
-	if GetConfigurationValue("AttemptsAtGoal").(int) > 3 {
-		fmt.Println("is this an int?")
+	if config.GetConfigurationValue("AttemptsAtGoal").(int) > 3 {
+		fmt.Println("OK")
 	}
 
-	err := SaveSpecified("config.json", "c:\\test")
+	err := config.SaveSpecified("config.json", "c:\\test")
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	enabled := ValueBool("IsTestEnabled")
+	enabled, _ := config.ValueBool("IsTestEnabled")
+
+	if !enabled {
+		log.Fatal("fail")
+	}
 
 	fmt.Println(enabled)
 
